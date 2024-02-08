9fa9c692dcdd2120a1f8371c8ea05b3907ac6534
ZachPrice <zach.price105@gmail.com>
2024-02-07 17:06:56 -0800

    new params

--- include/ethash/progpow.hpp
+++ include/ethash/progpow.hpp
@@ -19,12 +19,12 @@ using namespace ethash;  // Include ethash namespace.
 /// https://github.com/ifdefelse/ProgPOW#change-history.
 constexpr auto revision = "0.9.4";
 
-constexpr int period_length = 3;
-constexpr uint32_t num_regs = 8;
-constexpr size_t num_lanes = 16;
-constexpr int num_cache_accesses = 7;
-constexpr int num_math_operations = 11;
-constexpr size_t l1_cache_size = 12 * 1024;
+constexpr int period_length = 6;
+constexpr uint32_t num_regs = 16;
+constexpr size_t num_lanes = 8;
+constexpr int num_cache_accesses = 6;
+constexpr int num_math_operations = 9;
+constexpr size_t l1_cache_size = 8 * 1024;
 constexpr size_t l1_cache_num_items = l1_cache_size / sizeof(uint32_t);
 
 result hash(const epoch_context& context, int block_number, const hash256& header_hash,
